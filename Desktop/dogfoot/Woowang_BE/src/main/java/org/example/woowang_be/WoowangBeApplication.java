e6fb06951500637b1410d390d7349076c500105a
jjeongee <starstar117@naver.com>
2024-07-29 16:51:45 +0900

    default setting

---
+++ Desktop/dogfoot/Woowang_BE/src/main/java/org/example/woowang_be/WoowangBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.woowang_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowangBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoowangBeApplication.class, args);
+    }
+
+}
---
+++ Desktop/dogfoot/Woowang_BE/src/test/java/org/example/woowang_be/WoowangBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.woowang_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowangBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
